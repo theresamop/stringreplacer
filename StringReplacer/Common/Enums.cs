457f006c8c0ddb1d0510f629601e41953d154359
nuvasite <theresamop@yahoo.com>
2018-11-25 12:55:17 +0800

    added f

---
+++ StringReplacer/Common/Enums.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StringReplacer.Common
+{
+    public class Enums
+    {
+        public enum ReplacerType
+        {
+            SQLTableToCSClass = 1,
+        }
+
+    }
+
+    public static class CSDataTypeStrings
+    {
+        public const string Long = "long";
+        public const string nvarchar = "string";
+        public const string Char = "char";
+        public const string Int = "int";
+        public const string guid = "Guid";
+        public const string Bool = "bool";
+        public const string String = "string";
+        public const string datetime = "DateTime";
+    }
+
+    public static class SQLDataTypeStrings
+    {
+        public const string bigint = "bigint";
+        public const string nvarchar = "nvarchar";
+        public const string varchar = "varchar";
+        public const string Char = "char";
+        public const string datetime = "datetime";
+        public const string unique = "uniqueidentitifer";
+        public const string bit = "bit";
+        public const string Int = "int";
+    }
+}
---
+++ StringReplacer/Service/StringReplacerService.cs
@@ -0,0 +1,101 @@
+﻿using StringReplacer.Common;
+using StringReplacer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using static StringReplacer.Common.Enums;
+
+namespace StringReplacer.Service
+{
+    public class StringReplacerService
+    {
+
+        public byte[] ProcessString(StringReplacerViewModel vm)
+        {
+            string[] lines = System.IO.File.ReadAllLines(vm.FilePathInput);
+            System.IO.Stream stream = new System.IO.MemoryStream();
+            string sbuilder = "";
+
+            if ((int)ReplacerType.SQLTableToCSClass == vm.ReplacerType)
+            {
+                sbuilder = BuilDSQLTableToCSClass(vm, lines);
+            }
+
+            return Encoding.ASCII.GetBytes(sbuilder);
+
+        }
+
+        private string BuilDSQLTableToCSClass(StringReplacerViewModel vm, string[] lines)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            vm.StringMake = @"public {0} {1} {{get;set;}}";
+            foreach (string line in lines)
+            {
+
+                if (!string.IsNullOrEmpty(line))
+                {
+
+                    var stringSPlit = line.Split(' ');
+                    if (stringSPlit.Any() && stringSPlit[0] != null)
+                    {
+                        var isReqd = line.ToLower().Contains("not null");
+                        var newL = stringSPlit[0].Replace("\t", "").Replace("[", "").Replace("]", "");
+                        var type = stringSPlit[1].Any() ? stringSPlit[1].Replace("[", "").Replace("]", "") : "string";
+                        var datatype = GetDataType(type);
+                        string newString = string.Format(vm.StringMake, datatype + (isReqd ? "" : (datatype == CSDataTypeStrings.String ? "" : "?")), newL);
+
+                        stringBuilder.AppendLine(newString);
+                    }
+                }
+
+
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public string GetDataType(string type)
+        {
+            string datatype = CSDataTypeStrings.String;
+
+            switch (type)
+            {
+                case SQLDataTypeStrings.bigint:
+                    datatype = CSDataTypeStrings.Long;
+                    break;
+                case SQLDataTypeStrings.Int:
+                    datatype = CSDataTypeStrings.Int;
+                    break;
+                case SQLDataTypeStrings.nvarchar:
+                    datatype = CSDataTypeStrings.String;
+                    break;
+                case SQLDataTypeStrings.datetime:
+                    datatype = CSDataTypeStrings.datetime;
+                    break;
+                case SQLDataTypeStrings.bit:
+                    datatype = CSDataTypeStrings.Bool;
+                    break;
+                case SQLDataTypeStrings.Char:
+                    datatype = CSDataTypeStrings.Char;
+                    break;
+                case SQLDataTypeStrings.unique:
+                    datatype = CSDataTypeStrings.guid;
+                    break;
+            }
+            return datatype;
+        }
+
+        //public List<SelectListItem> GetAllReplacerTypes()
+        //{
+        //    List<SelectListItem> selectLists = new List<SelectListItem>()
+        //    {
+        //         new SelectListItem() { Text = "SQL Table to CS Class", Value = "1" }
+        //    };
+
+        //    return selectLists;
+        //}
+    }
+}
